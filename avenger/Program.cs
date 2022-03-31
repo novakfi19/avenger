8c02adafd039cfab0409c8f21044a14ef0ff19ad
Filip Novák <novakfi19@sps-prosek.cz>
2022-03-31 11:43:16 +0200

    31.3.

--- avenger/Program.cs
+++ avenger/Program.cs
@@ -1,7 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace avenger
 {
+    public class Demo
+    {
+        public event Action<int> VerejnaUdalost;
+        public event Func<int, string> VerejnaUdalost2;
+
+        public Demo()
+        {
+            VerejnaUdalost = Metoda1;
+
+            if (VerejnaUdalost != null)
+            {
+                VerejnaUdalost(1);
+            }
+
+            VerejnaUdalost2 = Metoda2;
+            VerejnaUdalost2?.Invoke(10);
+        }
+        public void Metoda1(int n)
+        {
+            Console.WriteLine(n);
+        }
+        public string Metoda2(int n)
+        {
+            return n.ToString();
+        }
+    }
     class Program
     {
 
@@ -25,13 +52,65 @@ namespace avenger
         }*/
         static void Main(string[] args)
         {
-            Avenger IronMan = new Avenger("Tony Stark");
+            /*Avenger IronMan = new Avenger("Tony Stark");
             Weapon RocketSuit = new Weapon("Rocket suit");
 
             RocketSuit.WeaponEquipped += IronMan.OnWeaponEquipped;
 
             RocketSuit.Equip(IronMan);
+            */
+
+            MCServer server = new MCServer();
+            var player = new Player() { Name = "Gejmr" };
+            var broadcast = new Broadcast();
+            var world = new World();
+
+            server.PlayerConnected += broadcast.whenPlayerConnects;
+            server.PlayerConnected += world.Spawn;
+
+            server.Connect(player);
+
+            server.PlayerConnected += broadcast.whenPlayerDisconnects;
+
+            server.Disconnect(player);
         }
 
-}
+    }
+    class MCServer
+    {
+        List<Player> players = new List<Player>();
+        public event Action<Player> PlayerConnected;
+        public void Connect(Player p)
+        {
+            players.Add(p);
+            PlayerConnected?.Invoke(p);
+        }
+        public void Disconnect(Player p)
+        {
+            PlayerConnected?.Invoke(p);
+        }
+    }
+    class Broadcast
+    {
+        public void whenPlayerConnects(Player p)
+        {
+            Console.WriteLine($"{p.Name} has connected!");
+        }
+        public void whenPlayerDisconnects(Player p)
+        {
+            Console.WriteLine($"{p.Name} has disconnected!");
+        }
+    }
+    class Player
+    {
+        public string Name { get; set; }
+    }
+    class World
+    {
+        public void Spawn(Player p)
+        {
+            //...
+            Console.WriteLine($"{p.Name} has spawned!");
+        }
+    }
 }
